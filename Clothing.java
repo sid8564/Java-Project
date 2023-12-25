48ed91df0e92564493da557cf8e61eee57b2b765
sid8564 <99875796+sid8564@users.noreply.github.com>
2023-12-25 17:58:39 +0530

    Create Clothing.java

---
+++ Clothing.java
@@ -0,0 +1,68 @@
+public class Clothing extends Product{
+    private String size;
+    private String color;
+
+    Clothing(){
+        super(Consts.CLOTHING);
+
+        setSize("");
+        setColor("");
+    }
+
+    Clothing(Integer code,String name, int inventory, double price, String size, String color,String category){
+        super(code,name, inventory, price, Consts.CLOTHING,category);
+
+        setSize(size);
+        setColor(color);
+    }
+
+    Clothing(String[] array){
+        super(Integer.valueOf(array[0]),array[1], Integer.valueOf(array[2]),
+                Double.valueOf(array[3]), array[4],array[5]);
+
+        setSize(array[6]);
+        setColor(array[7]);
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String toCSV () {
+        String str;
+        str = getProductCode() + "," + getProductName() + "," + getInventoryCount()
+                + "," + getPricePerUnit()
+                + "," + getProductType()
+                + "," + getCategory()
+         + "," + getSize()
+         + "," + getColor()
+         + System.lineSeparator()
+         ;
+        return str;
+    }
+    
+    public String toString() {
+        String str;
+        str = "\tProduct Code: "+getProductCode() + 
+        "\n\tProduct Name: " + getProductName() + 
+        "\n\tIn Stock: " + getInventoryCount() +
+        "\n\tPrice: " + getPricePerUnit() +
+        "\n\tCategory: " + getCategory() +
+        "\n\tSize: " + getSize() +
+        "\n\tColor: " + getColor() 
+        ;
+        return str;
+    }
+}
