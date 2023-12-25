72479417bb2c5c6f0eda68ef82f8f793b7fb8f55
sid8564 <99875796+sid8564@users.noreply.github.com>
2023-12-25 17:57:39 +0530

    Create Accessories.java

---
+++ Accessories.java
@@ -0,0 +1,29 @@
+public class Accessories extends Product{
+
+    Accessories(){
+        super(Consts.ACCESSORIES);
+    }
+    
+    Accessories(Integer code, String name, int inventory, double price, String category){
+        super(code, name, inventory, price, Consts.ACCESSORIES, category);
+    }
+    
+    Accessories(String[] array){
+        super(Integer.valueOf(array[0]),array[1], Integer.valueOf(array[2]), 
+        Double.valueOf(array[3]), array[4], array[5]);
+    }
+
+    public String toCSV () {
+        String str;
+        str = getProductCode() + "," + getProductName() + "," + getInventoryCount() 
+         + "," + getPricePerUnit()
+                + "," + getProductType()
+                + "," + getCategory()
+         + ","
+         + ","
+         + System.lineSeparator()
+         ;
+        return str;
+    }
+    
+}
